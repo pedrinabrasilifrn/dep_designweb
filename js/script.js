4e920f3f44d865d336825c0e763ca0e0c5a2a627
Pedrina <pedrinabrasil@gmail.com>
2026-08-26 15:58:30 -0300

    sintexe basica

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 alert("Oi do externo");
+
+var num = parseFloat(prompt("Digite um valor"));
+if (num == 0){
+    alert(`${num} é zero`);
+}else if (num %2 != 0){
+    alert(`${num} é ímpar`);
+}else{
+    alert(`${num} é par`);    
+}
+
+alert("Fim");
