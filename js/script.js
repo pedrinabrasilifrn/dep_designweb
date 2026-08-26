04b64f505e56e7ea719f5a68c853eb96b55d9d47
Pedrina <pedrinabrasil@gmail.com>
2026-08-26 16:16:06 -0300

    exemplo 01

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
-alert("Oi do externo");
-
-var num = parseFloat(prompt("Digite um valor"));
-if (num == 0){
-    alert(`${num} é zero`);
-}else if (num %2 != 0){
-    alert(`${num} é ímpar`);
-}else{
-    alert(`${num} é par`);    
+//Declaro a constante viva como true
+const viva = confirm("Clique em ok se voce está viva.");
+//Crio um bloco de execucao que so é executado se viva igual a true
+if (viva){
+    //Declaro a variavel nasc e atribuo a ela o valor digitado como int
+    var nasc = parseInt(prompt("Digite seu ano de nascimento:"));
+    //Declaro a variavel nome e atribuo a ela o valor digitado (padrao str)
+    let nome = prompt("Digite seu nome: ");
+    //Declaro a variavel altura e atribuo a ela o valor digitado como float
+    let altura = parseFloat(prompt("Digite sua altura em metros:"))
+    //Declaro a variavel idade
+    let idade;
+    //atribuo o valor de idade igual ao resulado da expressao 
+    idade = 2026 - nasc;
+    //exibo msg com o nome digitado e a idade calculada
+    alert(`${nome} você tem ${idade} anos.`);
+}else {
+    //exibo msg dizendo que pessoa está morta
+    alert(`Você está morta.`);    
 }
-
-alert("Fim");
