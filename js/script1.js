bf02b0d0ae5a2c612254aed9ffb164eeffc312fe
Pedrina <pedrinabrasil@gmail.com>
2026-09-09 13:36:58 -0300

    operadores aritmeticos

---
+++ js/script1.js
@@ -0,0 +1,11 @@
+let n1 = prompt("Digite n1");
+n1 = Number(n1);
+let n2 = prompt("Digite n2");
+n2 = Number(n2);
+
+alert(`${n1} + ${n2} = ${n1 + n2}`);
+alert(`${n1} - ${n2} = ${n1 - n2}`);
+alert(`${n1} * ${n2} = ${n1 * n2}`);
+alert(`${n1} / ${n2} = ${n1 / n2}`);
+alert(`${n1} % ${n2} = ${n1 % n2}`);
+alert(`${n1} ** ${n2} = ${n1 ** n2}`);
