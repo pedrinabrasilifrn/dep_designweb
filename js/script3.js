69fcba8dab56aefb53846e45c79ce1801b08ee5c
Pedrina <pedrinabrasil@gmail.com>
2026-09-16 14:08:03 -0300

    func anonimas

---
+++ js/script3.js
@@ -0,0 +1,15 @@
+let saudacao_a = function(){
+    alert(`Olá Fulano, seja bem-vindo!`);
+}
+
+let saudacao_b = function(nome){
+    alert(`Olá ${nome}, seja bem-vindo!`);
+}
+
+let saudacao_c = function(nome="Desconhecido"){
+    alert(`Olá ${nome}, seja bem vindo!`);
+}
+
+let saudacao_d = function(nome="Desconhecido"){
+    return `Olá ${nome}, seja bem-vindo!`;
+}
