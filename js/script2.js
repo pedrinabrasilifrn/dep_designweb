efce25d8b2180f7cf972215e523e13731605c812
Pedrina <pedrinabrasil@gmail.com>
2026-09-16 13:59:05 -0300

    criando func seta

--- js/script2.js
+++ js/script2.js
@@ -1,15 +1,15 @@
-function saudacao_a(){
+let saudacao_a = () =>{
     alert(`Olá Fulano, seja bem-vindo!`);
 }
 
-function saudacao_b(nome){
+let saudacao_b = (nome) =>{
     alert(`Olá ${nome}, seja bem-vindo!`);
 }
 
-function saudacao_c(nome="Desconhecido"){
+let saudacao_c = (nome="Desconhecido") =>{
     alert(`Olá ${nome}, seja bem vindo!`);
 }
 
-function saudacao_d(nome="Desconhecido"){
+let saudacao_d = (nome="Desconhecido") =>{
     return `Olá ${nome}, seja bem-vindo!`;
 }
